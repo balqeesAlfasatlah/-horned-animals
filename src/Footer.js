483a0db0ddcfa1317e74df315f8a0275d748cb00
balqessAlfasatlah <BalqeesAlfasatlah@yahoo.com>
2021-08-02 01:11:57 +0300

    finish

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React , { Component } from 'react';
+
+
+class Footer extends Component{
+    render(){
+        return(
+            <div>
+               <footer>
+                   <p>Balqees Alfasatlah &copy; 2021</p>
+               </footer>
+            </div>
+        )
+    }
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import { from } from "responselike";
+import React , { Component } from 'react';
+
+
+class Header extends Component{
+    render(){
+        return(
+            <div>
+                <header>
+                    <h1> Horned Beasts</h1>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default Header;
